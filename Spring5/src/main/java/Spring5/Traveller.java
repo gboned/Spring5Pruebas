23bb827f31b36dc60e7c6bdd0e920bb1c60a37ea
gboned <gboned@cifpfbmoll.eu>
2020-03-06 13:18:46 +0100

    feat(Traveller): Añadir clase para viajero

---
+++ Spring5/src/main/java/Spring5/Traveller.java
@@ -0,0 +1,16 @@
+package Spring5;
+
+public class Traveller {
+	private String name;
+	private Vehicle vehicle = new Vehicle("0042GOD", "Opel Corsa");
+	
+	public Traveller (String name) {
+		this.name = name;
+	}
+	
+	public Double move(Integer km) {
+		System.out.println("Travelling " + km);
+		Double cost = vehicle.move(km);
+		return cost;
+	}
+}
