946e4654fcaba38bbd8e0c3c4ba4438568ce8e20
gboned <gboned@cifpfbmoll.eu>
2020-03-06 13:29:49 +0100

    refactor(Traveller): Quitada instancia de Vehicle

--- Spring5/src/main/java/Spring5/Traveller.java
+++ Spring5/src/main/java/Spring5/Traveller.java
@@ -3,13 +3,12 @@ package Spring5;
 public class Traveller {
 	/* Añadidos atributos del viajero */
 	private String name;
-	/* El viajero necesita crear una instancia de vehículo
-	 * porque por cada viajero, hay un vehículo.
-	 * */
-	private Vehicle vehicle = new Vehicle("0042GOD", "Opel Corsa");
+	/* Por cada viajero, hay un vehículo. */
+	private Vehicle vehicle;
 	/* Creado constructor de la clase viajero */
 	public Traveller (String name) {
 		this.name = name;
+		this.vehicle = vehicle;
 	}
 	/* Cuando se llama a su método move, en verdad se
 	 * llama al del vehículo.
