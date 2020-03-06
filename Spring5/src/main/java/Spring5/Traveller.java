cf6d3c054222091b0cad977b31ba6319d5b9000e
gboned <gboned@cifpfbmoll.eu>
2020-03-06 13:24:52 +0100

    style(Traveller): Añadir comentarios al código

--- Spring5/src/main/java/Spring5/Traveller.java
+++ Spring5/src/main/java/Spring5/Traveller.java
@@ -1,13 +1,19 @@
 package Spring5;
 
 public class Traveller {
+	/* Añadidos atributos del viajero */
 	private String name;
+	/* El viajero necesita crear una instancia de vehículo
+	 * porque por cada viajero, hay un vehículo.
+	 * */
 	private Vehicle vehicle = new Vehicle("0042GOD", "Opel Corsa");
-	
+	/* Creado constructor de la clase viajero */
 	public Traveller (String name) {
 		this.name = name;
 	}
-	
+	/* Cuando se llama a su método move, en verdad se
+	 * llama al del vehículo.
+	 * */
 	public Double move(Integer km) {
 		System.out.println("Travelling " + km);
 		Double cost = vehicle.move(km);
